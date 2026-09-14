3d725e6868275dae62420e93004e41b61047247f
mafaldamsmartinho <mafalda.msmartinho@gmail.com>
2026-09-14 16:54:27 +0100

    added simple unit tests for preprocessing and predict modules

---
+++ tests/test_predict.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+
+from src.predict import predict
+
+
+class FakeModel:
+    def predict(self, data):
+        return np.array([1, 0])
+
+
+def test_predict():
+
+    data = pd.DataFrame({
+        "age": [50, 30],
+        "bp": [90, 70],
+    })
+
+    model = FakeModel()
+
+    result = predict(model, data)
+
+    assert list(result["prediction"]) == [1, 0]
+    assert result.index.equals(data.index)
